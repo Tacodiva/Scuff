1c6d9ed3b8e01b1c99aedf966070978ce2a20e6a
Tacodude7729 <27910867+Tacodude7729@users.noreply.github.com>
2023-02-05 17:49:37 +1100

    Make project structure more traditional

--- src/rollup.config.mjs
+++ src/rollup.config.mjs
@@ -9,7 +9,7 @@ import css from 'rollup-plugin-css-only';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'client_bundle/index.ts',
+	input: 'src/index.ts',
 
 	onwarn: function (message) {
 		// Make rollup shut up about circular dependency 'issues'.
@@ -21,47 +21,29 @@ export default {
 		sourcemap: !production,
 		format: 'amd',
 		name: 'app',
-		file: 'build/client/bundle.js',
+		file: 'public/bundle/bundle.js',
 	},
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
-				// enable run-time checks when not in production
 				dev: !production
 			}
 		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({ dedupe: ['svelte'] }),
 		commonjs(),
 
 		typescript(
 			{
-				tsconfig: "client_bundle/tsconfig.json",
+				tsconfig: "src/tsconfig.json",
 				sourceMap: !production,
 				inlineSources: !production
 			}
 		),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		// !production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload({ watch: ['client_www', 'build/client'] }),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		// production && terser()
+		!production && livereload({ watch: ['public'] }),
 	],
 	watch: {
 		clearScreen: false
